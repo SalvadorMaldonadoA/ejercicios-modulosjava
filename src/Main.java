5e22ef26807cfc34c397582204d4e589dd93531c
SalvadorMaldonadoA <25680179@cuautla.tecnm.mx>
2025-12-06 00:38:02 -0600

    Add files via upload
    Diseña una clase que represente una Cuenta de Ahorros simple.
    
    Atributos: El número de cuenta, el nombre del titular y el saldo actual de la cuenta.
    
    Comportamiento 1: Un método para realizar un Depósito, recibiendo la cantidad a ingresar como parámetro y actualizando el saldo.
    
    Comportamiento 2: Un método para simular un Retiro. Debe recibir la cantidad y actualizar el saldo, pero solo si la cantidad a retirar no excede el saldo actual.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Ingresa el numero de cuenta: ");
+        int numeroCuenta= scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ingresa el nombre del titular: ");
+        String titular = scanner.nextLine();
+        System.out.println("Coloque la cantidad a depositar: ");
+        int deposito= scanner.nextInt();
+        System.out.println("=== Situacion de retiro ===");
+        System.out.println("Coloque la cantidad a retirar: ");
+        int retiro=scanner.nextInt();
+
+    CuentaDeAhorros cuenta = new CuentaDeAhorros(numeroCuenta, titular, 500);
+        System.out.println("Nombre del titular: " + cuenta.getTitular());
+        System.out.println("Numero de cuenta: " + cuenta.getNumCuenta());
+        System.out.println("Saldo actual: "+ cuenta.getSaldoActual());
+        System.out.println("Ha realizado un deposito. Su saldo actual es de: "+ cuenta.deposito(deposito));
+        System.out.println("Saldo actual: "+ cuenta.retiro(retiro));
+    }
+}
