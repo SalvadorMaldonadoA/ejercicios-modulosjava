f7b142ce4a50528ffd78aa040e45d4e6e3516c35
SalvadorMaldonadoA <25680179@cuautla.tecnm.mx>
2025-12-06 00:24:16 -0600

    Add files via upload
    Diseña una clase que modele un Producto dentro de un almacén.
    
    Atributos: La referencia de almacén, la cantidad actual en stock y el precio unitario de venta.
    
    Comportamiento 1: Un método para incrementar la cantidad en stock después de una entrada de mercancía, recibiendo la cantidad de la entrada como argumento.
    
    Comportamiento 2: Un método que calcule el Valor Total del stock de ese producto (cantidad * precio unitario).

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,4 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-
-        // 1. Crear un objeto ArticuloBibliografico (Instanciación)
-        // Se llama al constructor que tienes definido en tu otra clase.
-        ArticuloBibliografico articulo1 = new ArticuloBibliografico("978-000", "J.K. Rowling", true, 1997);
-
-        // 2. Probar una de las funciones (Llamada a un metodo)
-        boolean sePudoReservar = articulo1.reservar();
-
-        // 3. Mostrar el resultado (Acceso a un atributo con Getter)
-        System.out.println("ID del Artículo: " + articulo1.getId());
-
-        if (sePudoReservar) {
-            System.out.println("Resultado de la reserva: ¡Reservado!");
-        } else {
-            System.out.println("Resultado de la reserva: No se pudo reservar.");
-        }
-
-        // 4. Probar la otra función
-        // Asumiendo que el año actual es 2025
-        boolean esViejo = articulo1.esApto(2025);
-        System.out.println("¿Tiene más de 5 años de antigüedad?: " + esViejo);
-    }
-}
